5e3873b0104ab142c22fb2c32b66c1371ff22412
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-22 09:04:11 -0400

    Add initial styling to product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,5 @@
+import "../styles/products.css"
+
 export default function ProductCard({
     details
 }) {
--- src/routes/store.jsx
+++ src/routes/store.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import ProductCard from "../components/ProductCard";
+import "../styles/products.css"
 
 export default function Store({
     cart
