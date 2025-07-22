3a85d2b2086d7d1a268ffb24c49fe6d746094ee5
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-22 12:35:52 -0400

    Add quantity adjust to cart page

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,11 +1,20 @@
-import { removeFromCart } from "../CartHelpers";
+import { use } from "react";
+import { removeFromCart, editCartQuantity } from "../CartHelpers";
+
+import QuantitySelector from "./QuantitySelector";
+import { useState } from "react";
 
 export default function CartCard({
     details,
     cart,
     cartSetter
 }) {
-    console.log(details);
+    const [updatedQuantity, setUpdatedQuantity] = useState(details.quantity);
+
+    function adjustQuantity() {
+        editCartQuantity(details, updatedQuantity, cart, cartSetter);
+    }
+
     return (
     <div className="cart-card">
         <img src={details.image} alt={details.name} />
@@ -15,6 +24,15 @@ export default function CartCard({
             <p>Each: ${details.price}</p>
             <p>Subtotal: ${details.quantity * details.price}</p>
         </div>
+        <div className="quantity-update-cart">
+            <p>Adjust Quantity:</p>
+            <QuantitySelector 
+                quantity={updatedQuantity}
+                setQuantity={setUpdatedQuantity}
+            />
+            <button onClick={adjustQuantity}>Update</button>
+        </div>
+        
         <button onClick={() => removeFromCart(details, cart, cartSetter)}>Remove</button>
     </div>
     )
