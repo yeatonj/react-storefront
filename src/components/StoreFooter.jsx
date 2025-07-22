3f3fc02925ef6efe25173e9b8c309711e1d1b357
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-22 09:50:14 -0400

    Add website footer

---
+++ src/components/StoreFooter.jsx
@@ -0,0 +1,13 @@
+import "../styles/store-footer.css"
+
+export default function StoreFooter() {
+    return (
+        <footer>
+            <div>
+                <p>Site by yeatonj</p> <a href="https://github.com/yeatonj"><img src="/src/img/github-mark.svg" alt="github linkn" /></a>
+            </div>
+            <p>&#9679;</p>
+            <p>Made with <a href="https://fakestoreapi.com/">Fake Store API</a></p>
+        </footer>
+    )
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,6 +2,8 @@ import { Outlet, Link } from "react-router-dom";
 import StoreHeader from "../components/StoreHeader";
 import { useState } from "react";
 import Cart from "../components/Cart";
+import StoreFooter from "../components/StoreFooter";
+import "../styles/root.css"
 
 
 export default function Root() {
@@ -16,6 +18,7 @@ export default function Root() {
             <div className="contents">
                 <Outlet />
             </div>
+            <StoreFooter />
             <Cart 
                 show={showCart}
                 hideCallback={() => setShowCart(false)}
